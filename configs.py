e7a7a3d653fbb11253f44918eede7f13d3153c1e
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-01-23 16:17:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,12 +3,12 @@ from os import getenv
 class Config:
     API_ID = int(getenv("API_ID", "21419016"))
     API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
-    BOT_TOKEN = getenv("BOT_TOKEN", "xxxxxxxxxxxxxxx")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8460604576:AAGRvVwRo9IokV8JU2vzTyUJ7lROkH2IgNE)
 
     # Admin / Owner IDs
     SUDO = list(map(int, getenv(
         "SUDO",
-        "7554081592 xxxxxxxxxxxxxx 7564050858 5656436152"
+        "7554081592 5109980512 7564050858 5656436152"
     ).split()))
 
     MONGO_URI = getenv(
